c7bfc07a765e939f485ea7b5385726924747f5d3
Ward Feys <wardfeys@hotmail.com>
2026-02-27 10:09:03 +0100

    Fix: add dependencies and cors setup

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const jsonServer = require('json-server');
+const cors = require('cors'); // Belangrijk!
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000; // Render bepaalt zelf de poort
 
+server.use(cors()); // Dit zorgt dat Vercel bij je data mag
 server.use(middlewares);
 server.use(router);
+
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
     console.log(`JSON Server draait op poort ${port}`);
 });
